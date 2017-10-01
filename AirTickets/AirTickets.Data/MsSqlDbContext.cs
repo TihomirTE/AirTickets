84db58f33ec045409409a66be2496cfee2d3cb51
TihomirTE <tihomirteofilov@gmail.com>
2017-10-01 15:16:58 +0300

    Change in the project architecture

---
+++ AirTickets/AirTickets.Data/MsSqlDbContext.cs
@@ -0,0 +1,23 @@
+﻿using AirTickets.Data.Model;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTickets.Data
+{
+    public class MsSqlDbContext : IdentityDbContext<User>
+    {
+        public MsSqlDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static MsSqlDbContext Create()
+        {
+            return new MsSqlDbContext();
+        }
+    }
+}
--- AirTickets/AirTickets/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AirTickets.MvcApplication" Language="C#" %>
