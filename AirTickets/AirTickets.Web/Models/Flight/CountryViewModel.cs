7d469fbb736cf6e016927489834bb8ccf36cecc0
TihomirTE <tihomirteofilov@gmail.com>
2017-10-08 14:09:35 +0300

    Add CountryViewModel

---
+++ AirTickets/AirTickets.Web/Models/Flight/CountryViewModel.cs
@@ -0,0 +1,33 @@
+﻿using AirTickets.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AirTickets.Data.Model;
+
+namespace AirTickets.Web.Models.Flight
+{
+    public class CountryViewModel
+    {
+        public CountryViewModel()
+        {
+
+        }
+
+        public CountryViewModel(CountryModel country)
+        {
+            if (country != null)
+            {
+                this.Id = country.Id;
+                this.Name = country.Name;
+                this.Airports = country.Airports.Select(x => new AirportViewModel(x)).ToList();
+            }
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<AirportViewModel> Airports { get; set; }
+    }
+}
