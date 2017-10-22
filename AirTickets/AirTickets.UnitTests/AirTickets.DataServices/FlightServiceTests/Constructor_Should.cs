9a5113eba8578a1d5d01e50b6be8a07fb522991d
TihomirTE <tihomirteofilov@gmail.com>
2017-10-22 15:23:57 +0300

    Add Flight service unit tests

---
+++ AirTickets/AirTickets.UnitTests/AirTickets.DataServices/FlightServiceTests/Constructor_Should.cs
@@ -0,0 +1,47 @@
+﻿using AirTickets.Data.Contracts;
+using AirTickets.Data.Models;
+using AirTickets.DataServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace AirTickets.UnitTests.AirTickets.DataServices.FlightServiceTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ReturnsAnInstance_WhenBothParametersAreNotNull()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Flight>>();
+            var dbContextMock = new Mock<IAirTicketDbContextSaveChanges>();
+
+            // Act
+            FlightService flightService = new FlightService(wrapperMock.Object, dbContextMock.Object);
+
+            // Assert
+            Assert.IsNotNull(flightService);
+        }
+
+        [TestMethod]
+        public void ThrowException_WhenFlightSetWrapperIsNull()
+        {
+            // Arrange
+            var dbContextMock = new Mock<IAirTicketDbContextSaveChanges>();
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new FlightService(null, dbContextMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowException_WhenDbContextIsNull()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Flight>>();
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new FlightService(wrapperMock.Object, null));
+        }
+    }
+}
---
+++ AirTickets/AirTickets.UnitTests/AirTickets.DataServices/FlightServiceTests/GetById_Should.cs
@@ -0,0 +1,68 @@
+﻿using AirTickets.Data.Contracts;
+using AirTickets.Data.Models;
+using AirTickets.DataServices;
+using AirTickets.DataServices.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace AirTickets.UnitTests.AirTickets.DataServices.FlightServiceTests
+{
+    [TestClass]
+    public class GetById_Should
+    {
+        [TestMethod]
+        public void ReturnFlightModel_WhenThereIsAModelWithThePassedId()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Flight>>();
+            var dbContextMock = new Mock<IAirTicketDbContextSaveChanges>();
+            Guid? flightId = Guid.NewGuid();
+
+            wrapperMock.Setup(m => m.GetById(flightId.Value)).Returns(new Flight() { Id = flightId.Value });
+
+            FlightService flightService = new FlightService(wrapperMock.Object, dbContextMock.Object);
+
+            // Act
+            FlightModel flightModel = flightService.GetById(flightId);
+
+            // Assert
+            Assert.IsNotNull(flightModel);
+        }
+
+        [TestMethod]
+        public void ReturnNull_WhenIdIsNull()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Flight>>();
+            var dbContextMock = new Mock<IAirTicketDbContextSaveChanges>();
+
+            FlightService flightService = new FlightService(wrapperMock.Object, dbContextMock.Object);
+
+            // Act
+            FlightModel flightModel = flightService.GetById(null);
+
+            // Assert
+            Assert.IsNull(flightModel);
+        }
+
+        [TestMethod]
+        public void ReturnNull_WhenThereIsNoModelWithThePassedId()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Flight>>();
+            var dbContextMock = new Mock<IAirTicketDbContextSaveChanges>();
+            Guid? flightId = Guid.NewGuid();
+
+            wrapperMock.Setup(m => m.GetById(flightId.Value)).Returns((Flight)null);
+
+            FlightService flightService = new FlightService(wrapperMock.Object, dbContextMock.Object);
+
+            // Act
+            FlightModel flightModel = flightService.GetById(flightId);
+
+            // Assert
+            Assert.IsNull(flightModel);
+        }
+    }
+}
